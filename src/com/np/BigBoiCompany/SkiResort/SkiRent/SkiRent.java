4cd6b7a56e49486168f69fbde3f2c04c8818faee
nickmee16 <nickmee16@gmail.com>
2021-02-04 17:54:49 +0200

    More Modifications

--- src/com/np/BigBoiCompany/SkiResort/SkiRent/SkiRent.java
+++ src/com/np/BigBoiCompany/SkiResort/SkiRent/SkiRent.java
@@ -1,5 +1,6 @@
 package com.np.BigBoiCompany.SkiResort.SkiRent;
 
+import com.np.BigBoiCompany.SkiResort.SkiRent.Ski.Base.SkiInfo;
 import com.np.BigBoiCompany.SkiResort.SkiRent.Ski.Base.SkiSizeTypes;
 import com.np.BigBoiCompany.SkiResort.SkiRent.SkiShoes.Base.SkiShoes;
 import com.np.BigBoiCompany.SkiResort.SkiRent.SkiShoes.ShoeK2;
@@ -14,29 +15,31 @@ import java.util.ArrayList;
 public class SkiRent {
 
     private double profit = 0;
-    private ArrayList<Ski> availableSkis = new ArrayList<>();;
-    private ArrayList<SkiShoes> shoes = new ArrayList<>();;
-    private ArrayList<SkiShoes> availableShoes = new ArrayList<>();;
-    private ArrayList<SkiShoes> takenShoes = new ArrayList<>();;
+    private ArrayList<Ski> availableSkis = new ArrayList<>();
+    private ArrayList<SkiShoes> shoes = new ArrayList<>();
+    private ArrayList<SkiShoes> availableShoes = new ArrayList<>();
+    private ArrayList<SkiShoes> takenShoes = new ArrayList<>();
+    private ArrayList<SkiInfo> skiNew = new ArrayList<>();
+    private ArrayList<SkiInfo> skiNewTaken = new ArrayList<>();
 
 
     public SkiRent() {
         populateSki();
-        populateSkiShoes();
+//        populateSkiShoes();
     }
 
-    private void populateSkiShoes() {
-        for (int i = 36; i < 46; i++) {
-            SkiShoes shoe1 = new ShoeK2(i);
-            SkiShoes shoe2 = new ShoeRossignol(i);
-            SkiShoes shoe3 = new ShoeLine(i);
-            for (int y = 0; y < 2; y++) {
-                this.availableShoes.add(shoe1);
-                this.availableShoes.add(shoe2);
-                this.availableShoes.add(shoe3);
-            }
-        }
-    }
+//    private void populateSkiShoes() {
+//        for (int i = 36; i < 46; i++) {
+//            SkiShoes shoe1 = new ShoeK2(i);
+//            SkiShoes shoe2 = new ShoeRossignol(i);
+//            SkiShoes shoe3 = new ShoeLine(i);
+//            for (int y = 0; y < 2; y++) {
+//                this.availableShoes.add(shoe1);
+//                this.availableShoes.add(shoe2);
+//                this.availableShoes.add(shoe3);
+//            }
+//        }
+//    }
 
     private void populateSki() {
         Ski ski1 = new SkiK2();
@@ -52,6 +55,60 @@ public class SkiRent {
 
     }
 
+    public void buySki(SkiSizeTypes skiLength, int numberOfPairs) {
+        SkiInfo ski;
+        int id = skiNew.size() + 1;
+
+        for(int i = 0; i < skiNew.size() - 1; i++) {
+            if(skiNew.get(i).getId() + 1 != skiNew.get(i + 1).getId()) {
+                id = skiNew.get(i).getId() + 1;
+            }
+        }
+
+        if(skiLength.equals(SkiSizeTypes.VERY_SMALL)) {
+            ski = new SkiInfo(id, new SkiFishcher(), 100, 0);
+
+        } else if(skiLength.equals(SkiSizeTypes.SMALL)) {
+            ski = new SkiInfo(id, new SkiLine(), 100, 0);
+
+        } else if(skiLength.equals(SkiSizeTypes.MEDIUM)) {
+            ski = new SkiInfo(id, new SkiRossignol(), 100, 0);
+
+        } else if(skiLength.equals(SkiSizeTypes.LONG)) {
+            ski = new SkiInfo(id, new SkiK2(), 100, 0);
+
+        } else {
+            ski = new SkiInfo(id, new SkiVolkl(), 100, 0);
+        }
+
+        for (int i = 0; i < numberOfPairs; i++) {
+            skiNew.add(ski);
+        }
+    }
+
+    public void buyShoes(SkiBrands brand, int size, int numberOfPairs) {
+        SkiShoes shoe;
+
+        if(brand.equals(SkiBrands.K2)) {
+            shoe = new ShoeK2(size);
+
+        } else if(brand.equals(SkiBrands.LINE)) {
+            shoe = new ShoeLine(size);
+
+        } else if(brand.equals(SkiBrands.ROSSIGNOL)) {
+            shoe = new ShoeRossignol(size);
+
+        } else {
+            System.out.println("No shoes of this brand");
+            return;
+        }
+
+        for(int i = 0; i < numberOfPairs; i++) {
+            availableShoes.add(shoe);
+        }
+
+    }
+
     public double getProfit() {
         return profit;
     }
@@ -62,30 +119,54 @@ public class SkiRent {
     }
 
     public void rentSki(SkiSizeTypes skiLength) {
-        SkiSizeTypes localType = SkiSizeTypes.NONE;
-        double skiProfit = 0;
+        SkiInfo ski = null;
 
-        for(int i = 0; i < availableSkis.size(); i++) {
-            if(availableSkis.get(i).getLength() == skiLength) {
-                Ski localSki = availableSkis.get(i);
+        for(int i = 0; i < skiNew.size(); i++) {
 
-                System.out.println("You rented:");
-                localSki.getInfoSki();
-                skiProfit = localSki.getRentPricePerDay();
-                localType = localSki.getLength();
+            if(skiNew.get(i).getSki().getLength() == skiLength) {
+                ski = skiNew.get(i);
+                skiNew.remove(i);
+                skiNewTaken.add(ski);
                 break;
             }
         }
 
-        if (localType == SkiSizeTypes.NONE) {
-            System.out.println("There are no ski with length " + skiLength.getValue() +
-                    "\n" + "Please insert correct ski length");
+        if(ski == null) {
+            System.out.println("We do not have this size at the moment!");
+
         } else {
-            System.out.println("Price: " + Utility.formatNumber(skiProfit) + "$");
-            profit = profit + (skiProfit);
-            System.out.println();
+            System.out.println("Rented Ski info" + "\n"
+                                + "ski: " + ski.getSki().getBrand() + "\n"
+                                + "length: " + ski.getSki().getLength() + "\n"
+                                + "price: " + Utility.formatNumber(ski.getSki().getRentPricePerDay()) + "$");
         }
     }
+
+//    public void rentSki(SkiSizeTypes skiLength) {
+//        SkiSizeTypes localType = SkiSizeTypes.NONE;
+//        double skiProfit = 0;
+//
+//        for(int i = 0; i < availableSkis.size(); i++) {
+//            if(availableSkis.get(i).getLength() == skiLength) {
+//                Ski localSki = availableSkis.get(i);
+//
+//                System.out.println("You rented:");
+//                localSki.getInfoSki();
+//                skiProfit = localSki.getRentPricePerDay();
+//                localType = localSki.getLength();
+//                break;
+//            }
+//        }
+//
+//        if (localType == SkiSizeTypes.NONE) {
+//            System.out.println("There are no ski with length " + skiLength.getValue() +
+//                    "\n" + "Please insert correct ski length");
+//        } else {
+//            System.out.println("Price: " + Utility.formatNumber(skiProfit) + "$");
+//            profit = profit + (skiProfit);
+//            System.out.println();
+//        }
+//    }
 
     public void rentShoes(int shoeSize) {
         double shoeProfit = 0;
